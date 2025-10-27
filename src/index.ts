5c97d7376b1964576a2f218a6d3488708bd71a53
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2025-10-27 11:19:51 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// REV 1:1
+  // The Revelation of Jesus Christ, which God gave unto him, 
+    // to shew unto his servants things which must shortly come to pass;
+  // and he sent and signified it by his angel unto his servant John:
+
+// REV 1:2
+  // who (John) bare record of the word of God, 
+  // and of the testimony of Jesus Christ, and of all things that he saw.
